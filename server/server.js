2511c4e2e407fc6d6f920be36f5c16b4504874e0
toshko89 <todor_petkov@mail.bg>
2022-02-28 21:09:32 +0200

    mongoDB and cloudinary added

--- server/server.js
+++ server/server.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const path = require('path');
 const config = require('./config/config.js');
+const mongo = require('./config/mongo.js');
+const cloudinary = require('cloudinary').v2;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors(config.CORS));
+app.use(cookieParser());
 
+mongo()
+.then(()=>{
+  cloudinary.config(config.CLOUDINARY);
+  app.listen(config.PORT, () => console.log(`Server is listening on port ${config.PORT}`));
+})
+.catch((err)=>{
+  console.log(err);
+  throw new Error(err);
+});
