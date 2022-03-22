7cde6f69b524e6961509472d70a0ebecab32e5fe
toshko89 <todor_petkov@mail.bg>
2022-03-22 14:58:00 +0200

    .env added

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,38 @@
+const env = process.env.NODE_ENV || 'development';
 
 const config = {
   development: {
-    PORT: 3030,
-    DB_CONNECTION_STRING: "mongodb+srv://TodorPetkov:VeryStrongPass@food-corner.imc4v.mongodb.net/food-corner?retryWrites=true&w=majority",
+    PORT: process.env.PORT || 3030,
+    DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
     COOKIE_NAME: 'X-Authorization',
-    SECRET: 'My strong secret',
+    SECRET: process.env.SECRET,
     SALT_ROUNDS: 10,
     CLOUDINARY: {
-      cloud_name: 'dl72c1rli',
-      api_key: '587398618388114',
-      api_secret: 'p86tgv0IJxRETWEw2KsiiHLs4O8'
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET
     },
     CORS: {
       origin: ['http://localhost:3000'],
       credentials: true
     }
-  }
+  },
+  production: {
+    PORT: process.env.PORT || 80,
+    DB_CONNECTION: process.env.DB_CONNECTION_STRING,
+    COOKIE_NAME: 'X-Authorization',
+    SECRET: process.env.SECRET,
+    SALT_ROUNDS: 10,
+    CLOUDINARY: {
+      cloud_name: process.env.CLOUDINARY_NAME,
+      api_key: process.env.CLOUDINARY_KEY,
+      api_secret: process.env.CLOUDINARY_SECRET
+    },
+    CORS: {
+      origin: [],
+      credentials: true
+    }
+  },
 }
 
-module.exports = config['development'];
+module.exports = config[env];
